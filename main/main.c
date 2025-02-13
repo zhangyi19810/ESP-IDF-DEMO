f0156f6e5b7affaa338f60077fb4ea63db1db6f4
gongyijiong <1553921732@qq.com>
2025-02-13 14:14:36 +0800

    get mac

--- main/main.c
+++ main/main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include "esp_system.h"
+#include "esp_mac.h"
 
-void app_main(void)
-{
-    printf("Hello, World!\n");  
+void app_main() {
+    uint8_t mac[6];
+
+    // 获取默认的 MAC 地址（从 eFuse 读取）
+    esp_efuse_mac_get_default(mac);
+
+    // 打印 MAC 地址
+    printf("MAC Address: %02X:%02X:%02X:%02X:%02X:%02X\n",
+           mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
 }
